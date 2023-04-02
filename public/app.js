44da4f6f76dddbbf91e6bd8913f7e23dc64d87a7
Abhishek Panwar <imabhishek612@gmail.com>
2023-04-02 16:19:30 +0530

    structured files with mvc and added sql for bckend

--- public/app.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>hi</h1>");
-});
-app.listen(3000);
